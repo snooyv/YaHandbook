623edf7f6726f2cbef55b0773e435442c7f57aee
Alex <sterx0@gmail.com>
2022-12-31 09:48:39 +0300

    start less_F

---
+++ yahandbook/chapter5/lesson5_1/less_F.py
@@ -0,0 +1,37 @@
+# Классный прямоугольник 2.0
+import math
+
+
+class Rectangle:
+
+    def __init__(self, tuple1, tuple2):
+        self.ax, self.ay = tuple1
+        self.cx, self.cy = tuple2
+
+    def perimeter(self):
+        ab = self.light(self.ax, self.ay, self.ax, self.cy)
+        bc = self.light(self.ax, self.cy, self.cx, self.cy)
+        return f"{2 * (ab + bc):.2f}"
+
+    @staticmethod
+    def light(p1x, p1y, p2x, p2y):
+        dx = p1x - p2x
+        dy = p1y - p2y
+        return math.hypot(dx, dy)
+
+    def area(self):
+        ab = self.light(self.ax, self.ay, self.ax, self.cy)
+        bc = self.light(self.ax, self.cy, self.cx, self.cy)
+        return f"{ab * bc:.2f}"
+
+    def get_pos(self):
+        pass
+
+    def get_size(self):
+        pass
+
+    def move(self, dx, dy):
+        pass
+
+    def resize(self, width, height):
+        pass
