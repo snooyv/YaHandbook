e9b6e3626a3fe1512b2a6142602d3f32a3b1428b
Alex <sterx0@gmail.com>
2022-12-31 07:44:02 +0300

    CH5 Ls5.1 less_B - 2/3 test ok 3??

--- yahandbook/chapter5/lesson5_1/less_B.py
+++ yahandbook/chapter5/lesson5_1/less_B.py
@@ -4,10 +4,9 @@ import math
 
 class Point:
 
-    def __init__(self, x, y, l=0):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.l = l
 
     def move(self, dx, dy):
         self.x += dx
@@ -16,9 +15,4 @@ class Point:
     def length(self, p):
         dx = self.x - p.x
         dy = self.y - p.y
-        self.l = math.hypot(dx, dy)
-        return self.l
-
-    # @property
-    # def __str__(self):
-    #     return '(Point {0})'.format(self.l)
+        return f"{math.hypot(dx, dy):.2f}"
